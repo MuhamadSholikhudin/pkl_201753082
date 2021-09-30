8c06f1acfb22d59db254107147a4a929ffd8e933
MuhamadSholikhudin <41666877+MuhamadSholikhudin@users.noreply.github.com>
2021-09-30 18:24:12 +0700

    Update Surat_masuk.php

--- application/controllers/Surat_masuk.php
+++ application/controllers/Surat_masuk.php
@@ -76,4 +76,48 @@ $data['klasifikasi_surat'] = ['Umum','Pemerintahan'];
         $this->Model_surat_masuk->tambah_surat_masuk($data, 'surat_masuk');
         redirect('surat_masuk/index');
     }
+
+Public function aksi_edit(){
+$id_suratmasuk = $this->input->post('id_suratmasuk');
+        $id_instansi = $this->input->post('id_instansi');
+         $no_urut = $this->input->post('no_urut');
+         $tanggal_surat = $this->input->post('tanggal_surat');
+         $perihal = $this->input->post('perihal');
+         $sifat_surat = $this->input->post('sifat_surat');
+         $isi_ringkas = $this->input->post('isi_ringkas');
+         $catatan = $this->input->post('catatan');
+        $no_suratmasuk = $this->input->post('no_suratmasuk');
+        $index = $this->input->post('index');
+         $tanggal_teruskan = $this->input->post('tanggal_teruskan');
+         $klasifikasi_surat = $this->input->post('klasifikasi_surat');
+        // $status = $this->input->post('status');
+
+        $data = array(
+            'id_instansi'    =>  $id_instansi,
+             'no_urut' =>  $no_urut,
+             'tanggal_surat' =>  $tanggal_surat,
+            'perihal' =>  $perihal,
+             'sifat_surat'    =>  $sifat_surat,
+             'isi_ringkas' =>  $isi_ringkas,
+             'catatan' =>  $catatan,
+             'no_suratmasuk'    =>  $no_suratmasuk,
+             'index' =>  $index,
+             'tanggal_teruskan' =>  $tanggal_teruskan,
+             'klasifikasi_surat' =>  $klasifikasi_surat,
+             'status' =>  0
+        );
+
+        // var_dump($data);
+        // $this->db->insert('surat_keluar', $data);
+
+$where = [
+'id_suratmasuk' => $id_suratmasuk
+];
+
+        $this->Model_surat_masuk->update_data($where, $data, 'surat_masuk');
+        redirect('surat_masuk/index');
+
+
+}
+
 }
